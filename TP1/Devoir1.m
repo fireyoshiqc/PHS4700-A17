017c32fa8ca6d55b1d081439ac41c11a8317747b
fireyoshiqc <fire_yoshi_qc@hotmail.com>
2017-09-21 17:38:57 -0400

    Started angular acceleration function.

--- TP1/Devoir1.m
+++ TP1/Devoir1.m
@@ -3,8 +3,11 @@ function [pcmNL INL alphaNL]=Devoir1(AngRot, vangulaire, forces, posNL)
   main();
   % Centre de masse
   pcmNL = CentreDeMasse(AngRot, posNL);
+  display(pcmNL);
   % display(pcmNL);
   % Moment inertie
   INL = MomentInertie(AngRot, posNL);
   display(INL);
+  
+  alphaNL = AccelerationAngulaire(AngRot, vangulaire, forces, posNL);
 endfunction
--- TP1/MomentInertie.m
+++ TP1/MomentInertie.m
@@ -1,4 +1,4 @@
-function momentintertie = MomentInertie(AngRot, posNL)
+function moment_inertie = MomentInertie(AngRot, posNL)
   global navette_cyl_h;
   global navette_cone_h;
   global navette_cyl_r;
@@ -76,7 +76,7 @@ function momentintertie = MomentInertie(AngRot, posNL)
   booster_cyl_droite_I_G = booster_cyl_I + booster_cyl_masse_tonnes * MatriceTranslation(d_booster_cyl_droite);
   
   % Somme des moments d'inertie
-  fusee_I = (reservoir_cone_I_G + reservoir_cyl_oxygene_I_G +  reservoir_cyl_hydrogene_I_G +
+  global fusee_I = (reservoir_cone_I_G + reservoir_cyl_oxygene_I_G +  reservoir_cyl_hydrogene_I_G +
             navette_cone_I_G + navette_cyl_I_G +
             booster_cone_gauche_I_G + booster_cyl_gauche_I_G +
             booster_cone_droite_I_G + booster_cyl_droite_I_G);
@@ -88,7 +88,7 @@ function momentintertie = MomentInertie(AngRot, posNL)
   R_x = [1, 0, 0; 0, cos(AngRot), -sin(AngRot); 0, sin(AngRot), cos(AngRot)];
   R_x_inverse = inv(R_x);
   % Appliquer la rotation
-  I_rot = R_x * fusee_I * R_x_inverse;
+  moment_inertie = R_x * fusee_I * R_x_inverse;
   
 endfunction
 
