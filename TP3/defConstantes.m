43c96c95d5bf3f6fe9ace17cb1d2bb03ee32ca3a
fireyoshiqc <fire_yoshi_qc@hotmail.com>
2017-11-01 13:24:05 -0400

    Fichier de constantes devrait être fini.

---
+++ TP3/defConstantes.m
@@ -0,0 +1,21 @@
+function constantes = defConstantes()
+% ** Toutes les dimensions sont en mètres et les masses en kilogrammes **
+constantes = struct(
+  'autos',
+  struct(
+    'a',
+    struct(
+      'masse', 1540,
+      'long', 4.78,
+      'larg', 1.82,
+      'h', 1.8
+    ),
+    'b',
+    struct(
+      'masse', 1010,
+      'long', 4.23,
+      'larg', 1.6,
+      'h', 1.8
+    )
+  )
+);
