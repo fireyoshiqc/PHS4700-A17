022a55834a9425bc3c028584266a374e0c74d8b0
Yawen <sytta@live.ca>
2017-11-09 01:27:43 -0500

    Capable de detection de collision mais pas encore determiner en tout temps (dans le cas de plus d'un coin en collision) le coin et la surface en collision

--- TP3/enCollision.m
+++ TP3/enCollision.m
@@ -1,4 +1,4 @@
-function estEnCollision = enCollision(posCentreAutoA, orientationXYAutoA, posCentreAutoB, orientationXYAutoB)
+function [estEnCollision autoEnCollision coinEnCollision surfaceEnCollision] = enCollision(posCentreAutoA, orientationXYAutoA, posCentreAutoB, orientationXYAutoB)
   constantes = defConstantes();
     
   #     4____(3)__3
@@ -11,31 +11,101 @@ function estEnCollision = enCollision(posCentreAutoA, orientationXYAutoA, posCen
   longueurA = constantes.autos.a.long;
   largeurA = constantes.autos.a.larg;
   
-  coinsSansRotA = [posCentreAutoA(1)-longueurA/2, posCentreAutoA(1)+longueurA/2, posCentreAutoA(1)+longueurA/2, posCentreAutoA(1)-longueurA/2;
-                   posCentreAutoA(2)-largeurA/2, posCentreAutoA(2)-largeurA/2, posCentreAutoA(2)+largeurA/2, posCentreAutoA(2)+largeurA/2];
-  matriceRotA = [cos(orientationXYAutoA), -sin(orientationXYAutoA); sin(orientationXYAutoA), cos(orientationXYAutoA)];
-  coinsAvecRotA = [matriceRotA*coinsSansRotA(:,1), matriceRotA*coinsSansRotA(:,2), matriceRotA*coinsSansRotA(:,3), matriceRotA*coinsSansRotA(:,4)];
- 
-  display(coinsAvecRotA);  
-    
+  coinsSansRotA = [-longueurA/2, longueurA/2, longueurA/2, -longueurA/2;
+                   -largeurA/2, -largeurA/2,  largeurA/2,   largeurA/2];
+                   
+  
+  coinsAvecRotA = R(coinsSansRotA, orientationXYAutoA);
+  coinsTransA = [coinsAvecRotA(:,1) + posCentreAutoA(1:2), coinsAvecRotA(:,2) + posCentreAutoA(1:2), coinsAvecRotA(:,3) + posCentreAutoA(1:2), coinsAvecRotA(:,4) + posCentreAutoA(1:2) ]
+  
   longueurB = constantes.autos.b.long;
   largeurB = constantes.autos.b.larg;
-  coinsSansRotB = [posCentreAutoB(1)-longueurB/2, posCentreAutoB(1)+longueurB/2, posCentreAutoB(1)+longueurB/2, posCentreAutoB(1)-longueurB/2;
-                   posCentreAutoB(2)-largeurB/2, posCentreAutoB(2)-largeurB/2, posCentreAutoB(2)+largeurB/2, posCentreAutoB(2)+largeurB/2];
-  matriceRotB = [cos(orientationXYAutoB), -sin(orientationXYAutoB); sin(orientationXYAutoB), cos(orientationXYAutoB)];
-  coinsAvecRotB = [matriceRotB*coinsSansRotB(:,1), matriceRotB*coinsSansRotB(:,2), matriceRotB*coinsSansRotB(:,3), matriceRotB*coinsSansRotB(:,4)]; 
+  coinsSansRotB = [-longueurB/2, longueurB/2, longueurB/2, -longueurB/2;
+                   -largeurB/2, -largeurB/2,  largeurB/2,   largeurB/2];
+  coinsAvecRotB = R(coinsSansRotB, orientationXYAutoB);
+  coinsTransB = [coinsAvecRotB(:,1) + posCentreAutoB(1:2), coinsAvecRotB(:,2) + posCentreAutoB(1:2), coinsAvecRotB(:,3) + posCentreAutoB(1:2), coinsAvecRotB(:,4) + posCentreAutoB(1:2) ]
   
-  display(coinsAvecRotB);
+  
+  faceA = [1 2 3 4];
+  patch('Faces',faceA,'Vertices',transpose(coinsTransA),'EdgeColor',"blue",'FaceColor',"none",'LineWidth',2);
+  faceB = [1 2 3 4];
+  patch('Faces',faceB,'Vertices',transpose(coinsTransB),'EdgeColor',"red",'FaceColor',"none",'LineWidth',2);
+  axis([0,100,0,100]);
+  view(2);
+  grid on;
   
   # Déterminer les normales des 4 surfaces
-  normalesA = [normale(coinsAvecRotA(:,1), coinsAvecRotA(:,2)), normale(coinsAvecRotA(:,2), coinsAvecRotA(:,3)), normale(coinsAvecRotA(:,3), coinsAvecRotA(:,4)), normale(coinsAvecRotA(:,4), coinsAvecRotA(:,1))];
-               
-  normalesB = [normale(coinsAvecRotB(:,1), coinsAvecRotB(:,2)), normale(coinsAvecRotB(:,2), coinsAvecRotB(:,3)), normale(coinsAvecRotB(:,3), coinsAvecRotB(:,4)), normale(coinsAvecRotB(:,4), coinsAvecRotB(:,1))];
-               
-  display(normalesA);
-  display(normalesB);
+  normalesA = [normale(coinsTransA(:,1), coinsTransA(:,2)), normale(coinsTransA(:,2), coinsTransA(:,3)), normale(coinsTransA(:,3), coinsTransA(:,4)), normale(coinsAvecRotA(:,4), coinsAvecRotA(:,1))];
+  normalesB = [normale(coinsTransB(:,1), coinsTransB(:,2)), normale(coinsTransB(:,2), coinsTransB(:,3)), normale(coinsTransB(:,3), coinsTransB(:,4)), normale(coinsAvecRotB(:,4), coinsAvecRotB(:,1))];
+  
+  # Evaluer d = n * (r - q) pour tous les coins r de autoA sur les coins q de autoB avec les normales de autoB
+  coinsASurfaceB = SurfaceCoins(coinsTransA, coinsTransB, normalesB);
+  
+  # S'il y a une surface de B dont tous les coins de A sont au dessus (d > 0), il y a un plan de division, donc pas de collision
+  # i.e un col dont tous les d(i,j,k) > 0
+  display(coinsASurfaceB);
+  display((all(coinsASurfaceB > 0, 1)));
+  if any(all(coinsASurfaceB > 0, 1) > 0)
+    enCollision = false;
+    display("Aucun coin A est en collision avec surface B qq");
+    return;
+  endif
+  
+  # Verifier s'il y a un coin de A en dessous de tous les plans de B (d <0)
+  display("Verifier si un coin de A en dessous de tous els plans de B");
+  for row=1:size(coinsASurfaceB, 1)
+    display(all(coinsASurfaceB(row,:) < 0));
+    if all(coinsASurfaceB(row,:) < 0)
+      display(strcat("Coin A" , num2str(row) , " en collision avec une surface B"));
+      enCollision = true;
+      autoEnCollision = 1; # 1 pour A
+      coinEnCollision = row; # le numéro du coin de auto A
+      # Determiner la surface de collision
+      for col = 1:size(coinsASurfaceB, 2)
+        display(nnz(coinsASurfaceB(:, col) > 0));
+        if nnz(coinsASurfaceB(:, col) > 0) == 3 # les 3 autres coins de A sont au-dessus de surface col de B
+          surfaceEnCollision = col;
+          display(strcat("Coin A" , num2str(row) , " en collision avec une surface B" + num2str(col)));
+          return;
+        endif      
+      endfor 
+    endif
+  endfor
+  
+  coinsBSurfaceA = SurfaceCoins(coinsTransB, coinsTransA, normalesA);
+  
+  # S'il y a une surface de A dont tous les coins de B sont au dessus (d > 0), il y a un plan de division, donc pas de collision
+  # i.e un col dont tous les d(i,j,k) > 0
+  display(coinsBSurfaceA); 
+  display((all(coinsBSurfaceA > 0, 1)));
+  if any(all(coinsBSurfaceA > 0, 1) > 0)
+    enCollision = false;
+        display("Aucun coin B est en collision avec surface A qq");
+    return;
+  endif
   
   
+  # Verifier s'il y a un coin de B en dessous de tous les plans de A (d < 0)
+  display("Verifier si un coin de B en dessous de tous les plans de A");
+  for row=1:size(coinsBSurfaceA, 1)
+    display(all(coinsBSurfaceA(row,:) < 0));
+    if all(coinsBSurfaceA(row,:) < 0)
+      display(strcat("Coin B", num2str(row), " en collision avec une surface A"));
+      enCollision = true;
+      autoEnCollision = 2; # 1 pour A
+      coinEnCollision = row; # le numéro du coin de auto B
+      # Determiner la surface de collision
+      for col = 1:size(coinsBSurfaceA, 2)
+        display(nnz(coinsBSurfaceA(:, col) > 0));
+        if nnz(coinsBSurfaceA(:, col) > 0) == 3 # les 3 autres coins de B sont au-dessus de surface col de A
+          surfaceEnCollision = col;
+          display(strcat("Coin B" , num2str(row) , " en collision avec une surface A", num2str(col)));
+          return;
+        endif      
+      endfor 
+    endif
+  endfor
+   
 endfunction
 
 # sens antihoraire
@@ -43,4 +113,23 @@ function n = normale(coin1, coin2)
   z = -1;
   p = coin1 - coin2;
   n = transpose(z * [p(2), -p(1)] / norm(z*[p(2), -p(1)]));
-endfunction  
+endfunction 
+
+# rotation
+function R = R(points, angle, position)
+  rot = [cos(angle), -sin(angle); sin(angle), cos(angle)];
+  R = [rot*points(:,1), rot*points(:,2), rot*points(:,3), rot*points(:,4)]; 
+endfunction 
+
+# coins1surface2
+function surfaceCoins = SurfaceCoins(coinsTrans1, coinsTrans2, normales2)
+  surfaceCoins = magic(4);
+  
+  for row=1:size(surfaceCoins,1)
+    for col=1:size(surfaceCoins,2)
+          diff = coinsTrans1(:,row) - coinsTrans2(:,col);
+          prod = dot(normales2(:,col), diff);      
+          surfaceCoins(row, col) = prod;
+      endfor
+  endfor
+endfunction
