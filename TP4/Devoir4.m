0d4040be0f8592e0ad7669938450dbb7f532c88d
Erica Bugden <erica.bugden@gmail.com>
2017-11-26 13:36:20 -0500

    Started iteration structure

--- TP4/Devoir4.m
+++ TP4/Devoir4.m
@@ -3,7 +3,33 @@ function [xi yi zi face] = Devoir4 (nout, nin, poso, name = "Graphique de la tra
   
   constantes = defConstantes();
   
- 
+  # Angles en radians (put into the constants file)
+  anglePolaireInitial = 0
+  anglePolaireFinal = pi/2
+  angleAzimutalInitial = 
+  angleAzimutalFinal = 
+  
+  nAnglesPolaire = 
+  nAnglesAzimutal = 
+  
+  for nIterPolaire = 1:nAnglesPolaire
+    # Calculer l'angle polaire
+    anglePolaire = anglePolaireInitial + (2*nIterPolaire - 1)(anglePolaireFinal - anglePolaireInitial)/2*nIterPolaire
+    
+    for nIterAzimutal = 1:nAnglesAzimutal
+      # Calculer l'angle azimutal pour l'iteration
+      angleAzimutal = angleAzimutalInitial + (2*nIterAzimutal - 1)(angleAzimutalFinal - angleAzimutalInitial)/2*nIterAzimutal
+      
+      # Calculer le vecteur directeur initial
+      dInit = [sin(anglePolaire)*cos(angleAzimutal), sin(anglePolaire)cos(angleAzimutal), cos(anglePolaire)]
+      
+      # Determiner si le rayon entre dans le cylindre
+      (x - constantes.cylindre.centre(1))^2 + (y - constantes.cylindre.centre(2))^2 - (constantes.cylindre.rayon)^2 = 0
+      
+      
+      for ()
+      
+      
 endfunction
 
 
