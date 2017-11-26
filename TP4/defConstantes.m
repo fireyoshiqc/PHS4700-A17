c113cc4352cf0d4c96a24b7181412abc478332cf
Félix Boulet <fire_yoshi_qc@hotmail.com>
2017-11-26 12:18:43 -0500

    Constantes devraient être correctes

--- TP4/defConstantes.m
+++ TP4/defConstantes.m
@@ -1,22 +1,57 @@
 function constantes = defConstantes()
 % ** Toutes les dimensions sont en mètres et les masses en kilogrammes **
 constantes = struct(
-  'autos',
+  'cylindre',
   struct(
-    'a',
+    'rayon', 2,
+    'hauteur', 18,
+    'centre', [4;4;11]
+  ),
+  'faces',
+  struct(
+    'rouge',
     struct(
-      'masse', 1540,
-      'long', 4.78,
-      'larg', 1.82,
-      'h', 1.8
+      'centre', [3;4;14.5],
+      'hauteur', 5,
+      'largeur', 2,
+      'couleur', [1;1;0]
     ),
-    'b',
+    'cyan',
     struct(
-      'masse', 1010,
-      'long', 4.23,
-      'larg', 1.6,
-      'h', 1.8
+      'centre', [4;4;14.5],
+      'hauteur', 5,
+      'largeur', 2,
+      'couleur', [0;1;1]
+    ),
+    'verte',
+    struct(
+      'centre', [3.5;3;14.5],
+      'hauteur', 5,
+      'largeur', 1,
+      'couleur', [0;1;0]
+    ),
+    'jaune',
+    struct(
+      'centre', [3.5;5;14.5],
+      'hauteur', 5,
+      'largeur', 1,
+      'couleur', [1;1;0]
+    ),
+    'bleue',
+    struct(
+      'centre', [3.5;4;12],
+      'longueur', 2,
+      'largeur', 1,
+      'couleur', [0;0;1]
+    ),
+    'rouge',
+    struct(
+      'centre', [3.5;4;17],
+      'longueur', 2,
+      'largeur', 1,
+      'couleur', [1;0;1]
     )
+  )
   )
 );
 endfunction
