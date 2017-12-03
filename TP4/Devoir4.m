7f9c63d9ef6b511dc9e2ef34f81ac89692a0261c
Félix Boulet <fire_yoshi_qc@hotmail.com>
2017-12-03 02:43:04 -0500

    Les points sont dessinés, mais clairement il y a des problèmes avec les angles réfléchis/réfractés.

--- TP4/Devoir4.m
+++ TP4/Devoir4.m
@@ -7,14 +7,9 @@ function [xi yi zi face] = Devoir4 (nout, nin, poso, name = "Rayons")
   h = constantes.cylindre.hauteur;
   
   figure('name', name);
-  
-  grid on;
   [cx, cy, cz] = cylinder();
   surf(2*cx+rc(1), 2*cy+rc(2), h*cz+rc(3)-h/2, 'FaceAlpha', 0.0)
-   
-  #[s,t]=resoudreRayonCylindre(constantes, [0 0 5], [1, 1]/norm([1,1]))  #TODO Remove?
-  
-  # TODO Put angles into the constants file?
+
   # Angle Polaire (theta) : Descend de l'axe des z vers le plan xy
   # Angle Azimutal (phi) : Augmente dans le sens ccw de l'axe des x positif dans le plan xy
   # Angles en radians
@@ -27,8 +22,13 @@ function [xi yi zi face] = Devoir4 (nout, nin, poso, name = "Rayons")
     anglePolaireFinal = pi+atan((sqrt(rc(1)^2+rc(2)^2)-R)/(rc(3)-h/2-poso(3)))
   end
   
-  nAnglesAzimutal = 10;  # TODO Optimize
-  nAnglesPolaire = 10;
+  nAnglesAzimutal = 5;
+  nAnglesPolaire = 5;
+  xi = [];
+  yi = [];
+  zi = [];
+  ci = [];
+  face = [];
   
   for nIterAzimutal = 1:nAnglesAzimutal
     # Calculer l'angle azimutal
@@ -41,12 +41,13 @@ function [xi yi zi face] = Devoir4 (nout, nin, poso, name = "Rayons")
       # Calculer le vecteur directeur initial
       dInit = [sin(anglePolaire)*cos(angleAzimutal), sin(anglePolaire)*sin(angleAzimutal), cos(anglePolaire)];
       dInit = dInit/norm(dInit);
-
-      # TODO Put this in a function that returns the collision & what it collided with
+      #prisme = constantes.prisme;
+      #dInit = prisme.centre' - poso;
+      #dInit = dInit/norm(dInit);
+      
       # Determiner si le rayon touche au cylindre
-
       s = resoudreRayonCylindre(constantes, poso', dInit);
-      s(s<=0) = inf; # Eliminer les valeurs inferieures a zero
+      s(s<=0.01) = inf; # Eliminer les valeurs inferieures a zero, plus une erreur d'incertitude
       s = min(s);
       pointIntersection = poso+dInit*s;  # Point de collision potentiel avec le cote du cylindre
       intersecteAvecCoteCyl = true;
@@ -54,41 +55,29 @@ function [xi yi zi face] = Devoir4 (nout, nin, poso, name = "Rayons")
         # Collision valide avec le rayon du cylindre
         hold on;
         plot3([poso(1);pointIntersection(1)], [poso(2);pointIntersection(2)], [poso(3);pointIntersection(3)]);
-        axis square; 
-        view(3);
+        
       else
         # Verifier avec le haut et le bas du cylindre
-        #s = resoudreHautBasCylindre(constantes, poso, dInit);
         s = resoudreHautBasCylindre(constantes, poso', dInit);
         if (s > 1000000)  # Valeur arbitraire pour dire que le rayon n'a pas frappe le cylindre
           # Rejeter le rayon, ne devrait pas arriver si on choisit bien nos angles
-          display("Ray discarded");
-          display(dInit);
           continue
         else
           # Collision valide avec le haut ou le bas du cylindre
-          pointIntersection = poso'+dInit*s;  # Point de collision avec le cote du cylindre
+          pointIntersection = poso+dInit*s;  # Point de collision avec le cote du cylindre
           intersecteAvecCoteCyl = false;
           hold on;
           plot3([poso(1);pointIntersection(1)], [poso(2);pointIntersection(2)], [poso(3);pointIntersection(3)]);
-          axis square; 
-          view(3);
         endif
       endif
       
-      # Si le rayon touche au cylindre
-      #posObservateur = poso;
-      #pointIntersection = [zst(1), zst(2), zst(3)];  # TODO Just do transpose of the vertical vector instead?
-      
-      # Pas necessaire, s c'est deja la distance parcourue
-      #distanceParcourue = norm(pointIntersection - posObservateur);
-      
       if intersecteAvecCoteCyl
         # Intersection avec le cote du cylindre
-        # Determiner la normale d'une surface tangante au cylindre au point d'intersection (i)
+        # Determiner la normale d'une surface tangente au cylindre au point d'intersection (i)
         centreDuCercle = [rc(1), rc(2), pointIntersection(3)];
         i = (pointIntersection - centreDuCercle);
         i = i/norm(i);
+        
       else
         # Intersection avec le haut ou le bas du cylindre
         if (pointIntersection(3) > rc(3))
@@ -104,6 +93,8 @@ function [xi yi zi face] = Devoir4 (nout, nin, poso, name = "Rayons")
       # Calculer la normale du plan d'incidence (j)
       j = cross(dInit, i);
       j = j/norm(j);
+      #hold on;
+      #plot3([pointIntersection(1);pointIntersection(1)+j(1)], [pointIntersection(2);pointIntersection(2)+j(2)], [pointIntersection(3);pointIntersection(3)+j(3)]);
       
       # Determiner l'angle d'incidence du rayon
       k = cross(i, j);
@@ -120,8 +111,6 @@ function [xi yi zi face] = Devoir4 (nout, nin, poso, name = "Rayons")
       endif
       
       # Calculer la direction refracte du rayon
-      # TODO Need to give better variable names
-      # TODO Check if the new direction calculation works
       angleTransmis = asin((nLiquide/nCylindre)*sin(angleIncidence));
       dApres = -i*cos(angleTransmis) + k*sin(angleTransmis);
       dApres = dApres/norm(dApres);
@@ -130,15 +119,27 @@ function [xi yi zi face] = Devoir4 (nout, nin, poso, name = "Rayons")
       
       
       # A l'interieur du cylindre
-      for nReflectTotInterne = 1:100 # On se limite a 100 reflexions internes
+      for nReflectTotInterne = 1:100 # On se limite a 100 reflexions internes (ne pas rouler ca avec les lignes de debug!!)
         dRayonIncident = dApres;
         
+        [s, couleur, numface] = resoudrePrisme(constantes, pointIntersection', dRayonIncident);
+        if (s < 1000000)
+          distanceTotale += s;
+          pointDessin = poso + distanceTotale*dInit;
+          xi = [xi; pointDessin(1)];
+          yi = [yi; pointDessin(2)];
+          zi = [zi; pointDessin(3)];
+          ci = [ci; couleur];
+          face = [face; numface];
+          hold on;
+          plot3([pointIntersection(1);pointIntersection(1)+s*dRayonIncident(1)], [pointIntersection(2);pointIntersection(2)+s*dRayonIncident(2)], [pointIntersection(3);pointIntersection(3)+s*dRayonIncident(3)]);
+          break;
+        endif
+        
         # Calculer le prochain point d'intersection
-        #prochainPointIntersection = [0, 0, 0];  # TODO Apply magic courbe parametrique fuckery
-        intersecteAvecCoteCyl = true;  # TODO Change to appropriate value when add intersection logic
-        # TODO Change boolean for indicating what you collided with to an integer? (enum?)
+        intersecteAvecCoteCyl = true;
         s = resoudreRayonCylindre(constantes, pointIntersection', dRayonIncident);
-        s(s<=0) = inf; # Eliminer les valeurs inferieures a zero
+        s(s<=0.01) = inf; # Eliminer les valeurs inferieures a zero, plus une erreur d'incertitude
         s = min(s);
         prochainPointIntersection = pointIntersection + s*dRayonIncident;  # Point de collision potentiel avec le cote du cylindre
         
@@ -149,11 +150,11 @@ function [xi yi zi face] = Devoir4 (nout, nin, poso, name = "Rayons")
         plot3([pointIntersection(1);prochainPointIntersection(1)], [pointIntersection(2);prochainPointIntersection(2)], [pointIntersection(3);prochainPointIntersection(3)]);
       else
         # Verifier avec le haut et le bas du cylindre
-        #s = resoudreHautBasCylindre(constantes, poso, dInit);
-        s = resoudreHautBasCylindre(constantes, pointIntersection', dRayonIncident);
+        s = resoudreHautBasCylindre(constantes, pointIntersection, dRayonIncident);
         if (s > 1000000)  # Valeur arbitraire pour dire que le rayon n'a pas frappe le cylindre
           # Rejeter le rayon, ne devrait pas arriver si on choisit bien nos angles
-          #display("Ray discarded");
+          #hold on;
+          #plot3([pointIntersection(1);pointIntersection(1)+10*dRayonIncident(1)], [pointIntersection(2);pointIntersection(2)+10*dRayonIncident(2)], [pointIntersection(3);pointIntersection(3)+10*dRayonIncident(3)]);
           break;
         else
           # Collision valide avec le haut ou le bas du cylindre
@@ -163,22 +164,13 @@ function [xi yi zi face] = Devoir4 (nout, nin, poso, name = "Rayons")
           plot3([pointIntersection(1);prochainPointIntersection(1)], [pointIntersection(2);prochainPointIntersection(2)], [pointIntersection(3);prochainPointIntersection(3)]);
         endif
       endif
-%        intersecteAvecCoteCyl = true;
-%
         # Calculer la distance parcourue
         distanceTotale += s;
         pointIntersection = prochainPointIntersection;
         
-        # TODO Fonction to determine which side of the object it collided with
-        # Si le rayon a touche a l'objet
-        #if ()
-          # TODO Mettre la couleur appropriee and go to the next rayon (push in the vector of points)
-          # (the distance is already accurate & dInit has the initial direction)
-          # break;
-        
         # Si le rayon touche au cylindre il faut determiner l'angle d'incidence
         if intersecteAvecCoteCyl
-          # Determiner la normale d'une surface tangante au cylindre au point d'intersection (i)
+          # Determiner la normale d'une surface tangente au cylindre au point d'intersection (i)
           centreDuCercle = [rc(1), rc(2), prochainPointIntersection(3)];
           i = (centreDuCercle - prochainPointIntersection);
           i = i/norm(i);
@@ -204,8 +196,9 @@ function [xi yi zi face] = Devoir4 (nout, nin, poso, name = "Rayons")
         
         if (estTransmise(nCylindre, nLiquide, angleIncidence))
           # Rejeter le rayon
-          #display("Ray discarded");
-          break
+          #hold on;
+          #plot3([prochainPointIntersection(1)], [prochainPointIntersection(2)], [prochainPointIntersection(3)], '.', 'MarkerSize', 20);
+          break;
         endif
         
         # Si le rayon est reflechie dans le cylindre calculer le prochain vecteur directeur
@@ -214,6 +207,15 @@ function [xi yi zi face] = Devoir4 (nout, nin, poso, name = "Rayons")
       end
     end
   end
+  
+  hold on;
+  scatter3(xi, yi, zi, 200, ci, '.');
+  #set(gca, 'CameraPosition', poso);
+  #set(gca, 'CameraTarget', constantes.cylindre.centre');
+  axis([0 20 0 20 0 20]); 
+  view(3);
+  grid on;
+  
 endfunction
 
 function estRefl = estReflechie(nInit, nFinal, angleInit)
@@ -229,17 +231,7 @@ function s = resoudreRayonCylindre(constantes, r0, u)
   R = constantes.cylindre.rayon;
   rc = constantes.cylindre.centre;
   d = [r0(1:2)-rc(1:2)];
-  # Si on part de r = r0 + su, on a X = r0(1) + su(1) et Y = r0(2) + su(2)
-  # Ensuite, on cherche a trouver les intersections avec (x-h)^2+(y-k)^2=R^2
-  # Si on remplace dans l'equation, on a : (r0(1)+su(1)-h)^2+(r0(2)+su(2)-h)^2=R^2
-  # En developpant, on a : (r0(1)^2+(su(1))^2+h^2+2r0(1)su(1)-2r0(1)h-2su(1)h) + (r0(2)^2+(su(2))^2+k^2+2r0(2)su(2)-2r0(2)k-2su(2)k) = R^2
-  # On peut regrouper certains termes : s^2*(u(1)^2+u(2)^2) -> s^2(u.u) [produit scalaire]
-  # Aussi : r0(1)^2 + r0(2)^2 - 2r0(1)h - 2r0(2)k + h^2 + k^2 -> (r0(1)-h)^2 + (r0(2)-k)^2 -> (d.d) ou d = r0(1:2) - rc(1:2) [qui equivaut a (r0(1)-h, r0(2)-k)]
-  # Finalement : 2r0(1)su(1)-2su(1)h + 2r0(2)su(2)-2su(2)k -> 2*(r0(1)su(1)-su(1)h+r0(2)su(2)-su(2)k) ->
-  # 2*([r0(1)-h, r0(2)-k].[su(1), su(2)]) -> 2*(d.u)
-  # On a finalement 0 = s^2*(u.u)+2s*(d.u)+((d.d)-R^2)
   s = roots([dot(u(1:2),u(1:2)), 2*dot(d,u(1:2)), dot(d,d)-R^2]);
-  
 endfunction
 
 function s = resoudreHautBasCylindre(constantes, r0, u)
@@ -249,17 +241,46 @@ function s = resoudreHautBasCylindre(constantes, r0, u)
   s = Inf;
   if (u(3)!=0)
     shaut = (rc(3)+h/2-r0(3))/u(3);
-    sbas = (rc(3)-h/2-r0(3))/u(3); 
-    if (shaut >= 0 && (norm((r0+shaut*u)-(rc+[0;0;h/2]))) <= R)
+    sbas = (rc(3)-h/2-r0(3))/u(3);
+    dist = norm((r0+shaut*u)-(rc+[0;0;h/2])');
+    if (shaut >= 0 && dist <= R)
       % Collision valide avec le haut du cylindre
       s = min(s, shaut);
     endif
-    if (sbas >= 0 && (norm((r0+sbas*u)-(rc-[0;0;h/2]))) <= R)
+    dist = norm((r0+sbas*u)-(rc-[0;0;h/2])');
+    if (sbas >= 0 && dist <= R)
       % Collision valide avec le bas du cylindre
       s = min(s, sbas);
     endif
   endif
   
+endfunction
+
+function [sm, color, numface] = resoudrePrisme(constantes, r0, u)
+  prisme = constantes.prisme;
+  faces = fieldnames(constantes.faces);
+  color = [0 0 0];
+  sm = inf;
+  numface = 0;
+  for i = 1:numel(faces)
+    face = constantes.faces.(faces{i});
+    rc = face.centre;
+    n = face.normale;
+    r = rc - r0;
+    s = dot(n,r)/dot(n,u);
+    pointIntersection = r0' + s*u;
+    if ((s > 0) && (s < sm) &&
+        (pointIntersection(1) >= prisme.centre(1)-prisme.dimensions(1)/2) &&
+        (pointIntersection(1) <= prisme.centre(1)+prisme.dimensions(1)/2) &&
+        (pointIntersection(2) >= prisme.centre(2)-prisme.dimensions(2)/2) &&
+        (pointIntersection(2) <= prisme.centre(2)+prisme.dimensions(2)/2) &&
+        (pointIntersection(3) >= prisme.centre(3)-prisme.dimensions(3)/2) &&
+        (pointIntersection(3) <= prisme.centre(3)+prisme.dimensions(3)/2))
+        color = face.couleur;
+        sm = s;
+        numface = i;
+     endif
+  end
 endfunction
 
 function dessinerGraphique(constantes, positions, rotations, name)
--- TP4/defConstantes.m
+++ TP4/defConstantes.m
@@ -7,49 +7,48 @@ constantes = struct(
     'hauteur', 18,
     'centre', [4;4;11]
   ),
+  'prisme',
+  struct(
+    'centre', [3.5;4;14.5],
+    'dimensions', [1;2;5]
+  ),
   'faces',
   struct(
     'rouge',
     struct(
       'centre', [3;4;14.5],
-      'hauteur', 5,
-      'largeur', 2,
-      'couleur', [1;1;0]
+      'couleur', [1 0 0],
+      'normale', [-1;0;0]
     ),
     'cyan',
     struct(
       'centre', [4;4;14.5],
-      'hauteur', 5,
-      'largeur', 2,
-      'couleur', [0;1;1]
+      'couleur', [0 1 1],
+      'normale', [1;0;0]
     ),
     'verte',
     struct(
       'centre', [3.5;3;14.5],
-      'hauteur', 5,
-      'largeur', 1,
-      'couleur', [0;1;0]
+      'couleur', [0 1 0],
+      'normale', [0;-1;0]
     ),
     'jaune',
     struct(
       'centre', [3.5;5;14.5],
-      'hauteur', 5,
-      'largeur', 1,
-      'couleur', [1;1;0]
+      'couleur', [1 1 0],
+      'normale', [0;1;0]
     ),
     'bleue',
     struct(
       'centre', [3.5;4;12],
-      'longueur', 2,
-      'largeur', 1,
-      'couleur', [0;0;1]
+      'couleur', [0 0 1],
+      'normale', [0;0;-1]
     ),
-    'rouge',
+    'magenta',
     struct(
       'centre', [3.5;4;17],
-      'longueur', 2,
-      'largeur', 1,
-      'couleur', [1;0;1]
+      'couleur', [1 0 1],
+      'normale', [0;0;1]
     )
   )
 );
